2edc0ab804ae3fd6cfde9e43a0bdf8032a2e1611
Satish Viswanathan <satish.viswanathan.in@gmail.com>
2024-04-01 13:28:47 +0530

    Added bufferline and Unimpaired plugins

--- lua/custom/plugins/lf.lua
+++ lua/custom/plugins/lf.lua
@@ -6,7 +6,7 @@ return {
     { 'akinsho/toggleterm.nvim', config = true },
   },
   config = function()
-    require('lf').setup()
+    require('lf').setup {}
   end,
   keys = { -- load the plugin only when using it's keybinding:
     { '<leader><leader>', '<cmd>Lf<cr>', desc = 'Open Lf file manager' },
